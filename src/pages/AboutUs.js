5388c2bc80e68ae293a72ad73572100951308004
joelleakilimali <akilimalijoelle@gmail.com>
2023-09-23 15:52:21 +0300

    fix  about us page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -11,84 +11,81 @@ const AboutUsPage = () => {
         <Navbar />
       </div>
       <div className="bg-gradient-to-r from-black to-gray-900 min-h-screen text-white">
-        <h1 className="heading p-10 text-yellow-400  ">A propos de Nous</h1>
-        <div className="content">
-          <div className="flex flex-col  ">
-            <h2 className="section-heading text-yellow-400">
+        <h1 className="heading p-5 text-yellow-400 text-3xl text-center">
+          A propos de Nous
+        </h1>
+        <div className="content mx-5 my-10">
+          <div className="mb-10">
+            <h2 className="section-heading text-yellow-400 text-2xl">
               Qui sommes-nous ?
             </h2>
-            <p className=" text-xl mx-48">
-              Basaliservice Agence événementielle, est une agence créé par un
-              groupe des jeunes ambitueux et motivés pour le changement. Née
-              d'une idée tirée d'ne prestation lors d'une soirée dansante d'un
+            <p className="text-xl">
+              Basaliservice Agence événementielle, est une agence créée par un
+              groupe de jeunes ambitieux et motivés pour le changement. Née
+              d'une idée tirée d'une prestation lors d'une soirée dansante d'un
               mariage d'un jeune couple en 2021. C'est après cette prestation
-              que nous jeunes, avions souhaité de créer cette agence.
+              que nous jeunes, avons souhaité créer cette agence.
             </p>
           </div>
-          <div className="flex flex-col  ">
-            <h2 className="section-heading text-yellow-400">Notre Mission </h2>
-            <p className="  text-xl mx-48">
+          <div className="mb-10">
+            <h2 className="section-heading text-yellow-400 text-2xl">
+              Notre Mission
+            </h2>
+            <p className="text-xl">
               Notre mission est d'assurer et de vous accompagner dans la
               réussite de toutes vos activités en assurant la couverture des
               services tels que le protocole, la sécurité; le maître de
               cérémonie, fille d'honneur et la décoration.
             </p>
           </div>
-
-          <h2 className="section-heading text-yellow-400">Nos valeurs</h2>
-          <ul className="text-xl">
-            <li>
-              Excellence: Nous nous engageons à offrir l'excellence dans tout ce
-              que nous faisons.
-            </li>
-            <li>
-              Integrité: Nous opérons avec le plus haut niveau d'intégrité et
-              normes éthiques.
-            </li>
-            <li>
-              Collaboration: Nous croyons au pouvoir de la collaboration et
-              travail en équipe.
-            </li>
-            <li>
-              Innovation: Nous encourageons une culture de l'innovation et
-              adoptons des nouvelles idées.
-            </li>
-            <li>
-              Orientation client : Nous accordons la priorité aux besoins de nos
-              clients et nous nous efforçons d'apporter les meilleures
-              solutions.
-            </li>
-          </ul>
-          <div className="my-20" style={{ marginBottom: "40px" }}>
-            <h2
-              className=" text-bold text-yellow-400 text-3xl my-5 py-8"
-              style={{ marginBottom: "10px" }}
-            >
+          <div className="mb-10">
+            <h2 className="section-heading text-yellow-400 text-2xl">
+              Nos valeurs
+            </h2>
+            <ul className="text-xl">
+              <li>
+                <span className="text-yellow-400">&#10003;</span> Excellence:
+                Nous nous engageons à offrir l'excellence dans tout ce que nous
+                faisons.
+              </li>
+              <li>
+                <span className="text-yellow-400">&#10003;</span> Intégrité:
+                Nous opérons avec le plus haut niveau d'intégrité et de normes
+                éthiques.
+              </li>
+              <li>
+                <span className="text-yellow-400">&#10003;</span> Collaboration:
+                Nous croyons au pouvoir de la collaboration et du travail en
+                équipe.
+              </li>
+              <li>
+                <span className="text-yellow-400">&#10003;</span> Innovation:
+                Nous encourageons une culture de l'innovation et adoptons de
+                nouvelles idées.
+              </li>
+              <li>
+                <span className="text-yellow-400">&#10003;</span> Orientation
+                client : Nous accordons la priorité aux besoins de nos clients
+                et nous nous efforçons d'apporter les meilleures solutions.
+              </li>
+            </ul>
+          </div>
+          <div className="my-10">
+            <h2 className="text-bold text-yellow-400 text-3xl mb-5 py-2 text-center">
               Nos contacts
             </h2>
-            <div
-              style={{
-                display: "grid",
-                gridTemplateColumns: "repeat(auto-fit, minmax(150px, 1fr))",
-                gridGap: "20px",
-              }}
-            >
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
               <Image
                 src="./assets/BSprotocol13.jpg"
                 alt="Nos contacts"
-                style={{
-                  width: "20%",
-                  height: "100px",
-                  objectFit: "fill",
-                  marginLeft: "430px",
-                  marginBottom: "30px",
-                }}
+                className="w-full h-auto"
               />
-              {/* Add more images here */}
+              {/* Ajoutez plus d'images ici */}
             </div>
           </div>
         </div>
       </div>
+
       <Footer />
     </div>
   );
--- src/pages/Service.js
+++ src/pages/Service.js
@@ -62,7 +62,7 @@ function Service() {
       <div>
         <Navbar />
       </div>
-      <div
+      {/* <div
         className=" flex flex-col bg-gradient-to-r from-black to-gray-900 min-h-screen text-gray-200 relative"
         style={{
           alignItems: "center",
@@ -132,7 +132,69 @@ function Service() {
             </div>
           </div>
         </div>
+      </div> */}
+      <div className="flex flex-col bg-gradient-to-r from-black to-gray-900 min-h-screen text-gray-200">
+        <h1 className="text-white font-bold text-2xl py-5 text-center">
+          Nos Tarifs
+        </h1>
+        <div className="flex flex-wrap justify-center mx-3">
+          {listService.map((service, index) => (
+            <div
+              key={index}
+              className="w-full md:w-1/2 lg:w-1/3 p-3"
+              onMouseEnter={() => handleMouseEnter(service)}
+              onMouseLeave={handleMouseLeave}
+              onClick={() => handleClick(service)}
+            >
+              <div
+                className={`bg-gray-800 p-5 rounded-lg ${
+                  selectedService === service ? "bg-yellow-500" : ""
+                }`}
+              >
+                <h2 className="text-xl font-bold text-yellow-500">
+                  {service.service}
+                </h2>
+                <p>Prix Unitaire: ${service.prixUnitaire}</p>
+                <p>Prix pour 20 personnes: ${service.prixVintaine}</p>
+                <p>Prix pour 50 personnes: ${service.prixCinquantaine}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="p-24 text-center mx-10 bg-gray-800 rounded-lg shadow-lg ">
+          <h2 className="text-xl text-white">
+            Vous ne trouvez pas ce que vous cherchez dans notre tarif ?
+            Personnalisez le vôtre...
+          </h2>
+          <div className="text-lg my-5">
+            <div className="flex justify-center items-center p-3">
+              <h1 className="mx-2 text-white">Type de Service:</h1>
+              <input
+                className="w-full p-2 bg-gray-900 rounded text-white"
+                type="text"
+                placeholder="Enterrement de vie de jeune fille"
+              />
+            </div>
+            <div className="flex justify-center items-center p-3">
+              <h1 className="mx-2 text-white">Nombre de Personnes:</h1>
+              <input
+                className="w-full p-2 bg-gray-900 rounded text-white"
+                type="number"
+                placeholder="100"
+              />
+            </div>
+            <div className="flex justify-center items-center p-3">
+              <h1 className="mx-2 text-white">Type d'Espace:</h1>
+              <select className="w-full p-2 bg-gray-900 rounded text-white">
+                <option value="salle">Salle de fête</option>
+                <option value="espace_ouvert">Espace Ouvert</option>
+                <option value="autour_piscine">Autour de la piscine</option>
+              </select>
+            </div>
+          </div>
+        </div>
       </div>
+
       <div>
         <Footer />
       </div>
